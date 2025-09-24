747fcb3a0517d1c0ed6ba1ad35533a595bc9cd63
sureshdtrainer <suresh.d.trainer@gmail.com>
2025-09-24 17:26:08 +0530

    implemented get by id test case

--- tests/test_actvitiies.py
+++ tests/test_actvitiies.py
@@ -2,10 +2,26 @@ import pytest
 from utils.apis import APIS
 
 
-def test_get_all_activities():
-    apis = APIS()
+@pytest.fixture(scope="module")
+def apis():
+    return APIS()
+
+
+def test_get_all_activities(apis):
+    # apis = APIS()
 
     response = apis.get_all_activities()
+    print(response.status_code)
+    print(response.json())
+    print(response.headers)
+
+    assert response.status_code == 200
+
+
+def test_get_activity_id(apis):
+    # apis = APIS()
+
+    response = apis.get_activity_by_id(5)
     print(response.status_code)
     print(response.json())
     print(response.headers)
